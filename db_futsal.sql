a1e9d5a73cc78d45548a443bc796af2b3484615c
pepripratama <pepripratama@gmail.com>
2022-02-14 22:05:10 +0700

    upload database

---
+++ db_futsal.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Feb 2022 pada 16.04
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_futsal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_lapangan`
+--
+
+CREATE TABLE `tb_lapangan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `status` int(1) NOT NULL COMMENT '1 = tersedia, 2 = tidak'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_lapangan`
+--
+
+INSERT INTO `tb_lapangan` (`id`, `nama`, `status`) VALUES
+(1, 'Lapangan 1', 1),
+(2, 'Lapangan 2', 1),
+(3, 'Lapangan 3', 1),
+(4, 'Lapangan VIP 1', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_lapangan`
+--
+ALTER TABLE `tb_lapangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_lapangan`
+--
+ALTER TABLE `tb_lapangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
